2dd83771e7e55fab0efe82465446c8056fbf7d86
Malith-Anjana <malithanjana97@gmail.com>
2021-10-07 21:56:44 +0530

    Payment form add

--- app/src/main/java/com/example/cargillsshop/mpayment_form.java
+++ app/src/main/java/com/example/cargillsshop/mpayment_form.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class mpayment_form extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mpayment_form);
 
+        getSupportActionBar().hide();
     }
 }
