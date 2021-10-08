bc87e618b4cd07283104a6d588eb683f74044165
Malith-Anjana <malithanjana97@gmail.com>
2021-10-09 01:42:00 +0530

    payment form validation

--- app/src/main/java/com/example/cargillsshop/m_cartlist_Activity.java
+++ app/src/main/java/com/example/cargillsshop/m_cartlist_Activity.java
@@ -3,8 +3,12 @@ package com.example.cargillsshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class m_cartlist_Activity extends AppCompatActivity {
+    Button btn_plus1, btn_minus1,btn_check;
+    TextView txt_count1, text_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,9 @@ public class m_cartlist_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_mcartlist);
 
         getSupportActionBar().hide();
+
+
+
 
     }
 }
--- app/src/main/java/com/example/cargillsshop/mpayment_form.java
+++ app/src/main/java/com/example/cargillsshop/mpayment_form.java
@@ -8,11 +8,13 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class mpayment_form extends AppCompatActivity {
     Button pay_btn, done_btn;
     Dialog dialog;
+    EditText edit_person,edit_numb, edit_month, edit_yr, edit_cvc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +24,32 @@ public class mpayment_form extends AppCompatActivity {
 
         pay_btn = findViewById(R.id.payment_button);
         done_btn = findViewById(R.id.btn_Done);
+
+        edit_person=findViewById(R.id.edit_person);
+        edit_numb=findViewById(R.id.edit_cardnumb);
+        edit_month=findViewById(R.id.edit_month);
+        edit_yr=findViewById(R.id.edit_year);
+        edit_cvc=findViewById(R.id.edit_cvc);
+
         dialog= new Dialog(this);
 
         pay_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openWinDialog();
+                String name = edit_person.getText().toString();
+                String numb = edit_numb.getText().toString();
+                String month = edit_month.getText().toString();
+                String year = edit_yr.getText().toString();
+                String cvc = edit_cvc.getText().toString();
+
+                boolean check = validatePaymentDetails(name,numb,month,year,cvc);
+
+                if(check==true){
+                    openWinDialog(name,numb,month,year,cvc);
+                }
+                else{
+//                    Toast.makeText(mpayment_form.this, "Fill the form with valid inputs", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -36,18 +58,62 @@ public class mpayment_form extends AppCompatActivity {
     }
 
 
+    private Boolean validatePaymentDetails(String name, String numb, String month, String year, String cvc) {
 
-    private void openWinDialog(){
-    dialog.setContentView(R.layout.m_dialoguebox_success);
+
+        if (name.length() == 0) {
+            edit_person.requestFocus();
+            edit_person.setError("This Field Cannot be Empty");
+            return false;
+        }
+        if (numb.length() < 16 || numb.length() == 0) {
+            edit_numb.requestFocus();
+            edit_numb.setError("Card number requires 16 digits");
+            return false;
+        }
+        if (year.length() < 2 || year.length() == 0) {
+            edit_yr.requestFocus();
+            edit_yr.setError("Input the valid Year");
+            return false;
+        }
+        if (month.length() < 2 || month.length() == 0) {
+            edit_month.requestFocus();
+            edit_month.setError("Input the valid Month");
+            return false;
+
+        } if (cvc.length() < 3 || cvc.length() == 0) {
+            edit_cvc.requestFocus();
+            edit_cvc.setError("Input the valid CVC code");
+            return false;
+        }
+
+        else{
+            return true;
+        }
+    }
+
+
+
+
+    private void openWinDialog(String name,String numb, String yr, String month, String cvc){
+    dialog.setContentView(R.layout.activity_mdialoguebox_success);
     dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
 
-        Button btnDone = dialog.findViewById(R.id.btn_Done);
+        Button btn_no = dialog.findViewById(R.id.btn_no);
+        Button btn_yes = dialog.findViewById(R.id.btn_yes);
         dialog.show();
 
-        btnDone.setOnClickListener(new View.OnClickListener() {
+        btn_no.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
+            }
+        });
+
+        btn_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
