3e7269cd6093c9d833c9ac0a426ac54b77a2cb08
Malith-Anjana <malithanjana97@gmail.com>
2021-10-08 20:33:43 +0530

    Verification interface added

---
+++ app/src/main/java/com/example/cargillsshop/m_OTP_Activity.java
@@ -0,0 +1,16 @@
+package com.example.cargillsshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class m_OTP_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motp);
+
+        getSupportActionBar().hide();
+    }
+}
